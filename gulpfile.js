ca5ae09d9c5247ba0fb496d0f7ccb000c5ca016c
Chiara Zielinsky <chiara.zielinsky@gmail.com>
2017-04-07 18:48:04 -0400

    updating front page

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,9 @@ const plugin = autoprefixer({ grid: false });
 gulp.task('styles', () => {
   return gulp.src('./styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-     .pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    
     .pipe(concat('styles.css'))
+    .pipe(autoprefixer({browsers: ['last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'], grid: false}))
     .pipe(gulp.dest('./styles'))
      .pipe(reload({stream: true}));
 });
